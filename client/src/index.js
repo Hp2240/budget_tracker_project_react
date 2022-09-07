8b5625dd2d9d80d5652b3ee2a7d8ae36c1835bc5
Hyeree Park <hp2240@nyu.edu>
2022-09-06 22:34:22 -0400

    created react app

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/index.css'
+import App from './App'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+)
